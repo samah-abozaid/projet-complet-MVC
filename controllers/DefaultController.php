329ae733299718ac3749c7959527a9e5141b9bad
Barbara-Mase <mase.barbara@gmail.com>
2026-03-09 11:15:35 +0000

    ajout DefaultController

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -3,6 +3,38 @@
 class DefaultController extends AbstractController
 {
 
+    public function home() : void {
+
+    }
+    
+    public function match() : void {
+        
+    }
+    
+    public function matchs() : void {
+        
+    }
+    
+    public function player() : void {
+        
+    }
+    
+    public function players() : void {
+        
+    }
+    
+    public function team() : void {
+        
+    }
+    
+    public function teams() : void {
+        
+    }
+    
+    public function notfound() : void {
+        
+    }
+    
     
     
 }
