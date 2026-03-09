42ae93c30caa289f128c5ed7c5888f67355e6246
samah-abozaid <samah.abozaid89@gmail.com>
2026-03-09 10:56:59 +0000

    ajouter le services

--- services/Router.php
+++ services/Router.php
@@ -2,45 +2,36 @@
 
 class Router
 {
-    public function handleRequest() : void
+    public function handleRequest(): void
     {
-            
-        if(isset($_GET["route"])) {
-            
-           if($_GET["route"] === "match") {
-                $ctrl = new BlogController();
-                $ctrl -> match();
-           
-            
-          } else  if($_GET["route"] === "matchs") {
-            $ctrl = new BlogController();
-            $ctrl->games();
+        $ctrl = new DefaultController();
+
+        if (isset($_GET["route"])) {
+
+            if ($_GET["route"] === "match") {
+                $ctrl->match();
+
+            } elseif ($_GET["route"] === "matchs") {
+                $ctrl->games();
+
+            } elseif ($_GET["route"] === "player") {
+                $ctrl->player();
+
+            } elseif ($_GET["route"] === "players") {
+                $ctrl->players();
+
+            } elseif ($_GET["route"] === "team") {
+                $ctrl->team();
+
+            } elseif ($_GET["route"] === "teams") {
+                $ctrl->teams();
+
+            } else {
+                $ctrl->notfound();
+            }
+
+        } else {
+            $ctrl->home();
         }
-        
-        
-        } else if($_GET["route"] === "player") {
-            $ctrl = new BlogController();
-            $ctrl->player();
-          
-          
-        } else if($_GET["route"] === "players") {
-            $ctrl = new BlogController();
-            $ctrl->players(); 
-           
-        } else if($_GET["route"] === "team") {
-            $ctrl = new BlogController();
-            $ctrl->team();   
-            
-        } else if($_GET["route"] === "teams") {
-            $ctrl = new BlogController();
-            $ctrl->teams();
-          
-           
-            
-        
-    }else {
-          $ctrl = new DefaultController();
-               $ctrl->home();
     }
-}
 }
